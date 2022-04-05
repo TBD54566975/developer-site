31054c25c9a51006652340eab6ffa895cc6ea3e6
Daniel Buchner <danieljb2@gmail.com>
2022-04-04 21:48:27 -0700

    tidy up home page styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title" Style="white-space: nowrap">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         {/* <div className={styles.buttons}>
           <Link
